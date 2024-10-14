d99a18f578b4d4de05ff4fdadca3eb1d2ca3dd7c
박지원 <jackgg12322@gmail.com>
2024-10-14 20:15:39 +0900

    css:"mobile size 추가 (HomeButton.tsx)"

--- src/app/components/HomeButton.tsx
+++ src/app/components/HomeButton.tsx
@@ -11,10 +11,10 @@ const HomeButton = () => {
       custom-bg fixed top-4 left-4 w-fit self-start"
       aria-label={'home'}
     >
-      <span className="relative  w-14 h-14 p-4  text-black  group-hover:text-accent">
+      <span className="relative  w-14 h-14 p-4 sm:w-6 sm:h-6 sm:p-1 text-black  group-hover:text-accent">
         <Home className="w-full h-auto" />
         <span className="peer bg-transparent absolute top-0 left-0 w-full h-full" />
-        <span className="absolute hidden peer-hover:block px-2 py-1 left-full mx-2 top-1/2 -translate-y-1/2 bg-background text-black text-sm rounded-md shadow-lg whitespace-nowrap">
+        <span className="absolute hidden peer-hover:block px-2 sm:px-0 py-1 sm:py-0 sm:-translate-y-0  left-full sm:left-0 sm:bottom-full mx-2 sm:mx-0 top-1/2 sm:top-6 -translate-y-1/2 bg-background text-black text-sm sm:text-[0.8rem] rounded-md shadow-lg whitespace-nowrap">
           Home
         </span>
       </span>
