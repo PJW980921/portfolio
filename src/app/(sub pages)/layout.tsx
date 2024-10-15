2758d3b9667606d6256704807a1e99aaa2e392be
박지원 <jackgg12322@gmail.com>
2024-10-15 15:45:50 +0900

    feat:"channel talk service 추가"

--- src/app/(sub pages)/layout.tsx
+++ src/app/(sub pages)/layout.tsx
@@ -1,10 +1,20 @@
-import { ReactNode } from 'react';
+'use client';
+import { ReactNode, useEffect } from 'react';
 import HomeButton from '../components/HomeButton';
+import ChannelService from '../lib/ChannelService';
 
 interface SubPagesLayoutProps {
   children: ReactNode;
 }
+
 export default function SubPagesLayout({ children }: SubPagesLayoutProps) {
+  useEffect(() => {
+    ChannelService.loadScript();
+
+    ChannelService.boot({
+      pluginKey: process.env.NEXT_PUBLIC_CHANNEL_TALK_KEY as string,
+    });
+  }, []);
   return (
     <main className="flex min-h-screen flex-col items-center justify-center py-20 px-16">
       <HomeButton />
