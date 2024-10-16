cedb8f401c7314dae25f90e8b041fb18980a61e1
박지원 <jackgg12322@gmail.com>
2024-10-16 12:12:08 +0900

    feat: "FullPageScroll Slider Layout 적용(project)"

--- src/app/(sub pages)/projects/page.tsx
+++ src/app/(sub pages)/projects/page.tsx
@@ -1,3 +1,29 @@
+import { FullPageScroll } from '@/app/components/FullPageScrollLayout';
+import BlurFade from '@/app/components/ui/blur-fade';
+
 export default function Projects() {
-  return <div></div>;
+  return (
+    <FullPageScroll>
+      <section className="flex flex-col justify-center items-center">
+        <BlurFade
+          delay={0.25}
+          inView
+          className="flex flex-col justify-center items-center"
+        >
+          <h3 className="font-bmEuljiro text-[38px] pb-20">Projects</h3>
+          <h3 className="font-bmEuljiro text-[28px]">PJW WEB PortFolio</h3>
+        </BlurFade>
+      </section>
+      <section className="flex flex-col justify-center items-center">
+        <BlurFade delay={0.25} inView>
+          <h3 className="font-bmEuljiro text-[38px]">EmotionalCore</h3>
+        </BlurFade>
+      </section>
+      <section className="flex flex-col justify-center items-center gap-8   ">
+        <BlurFade delay={0.25} inView>
+          <h3 className="font-bmEuljiro text-[38px]">Taskify</h3>
+        </BlurFade>
+      </section>
+    </FullPageScroll>
+  );
 }
