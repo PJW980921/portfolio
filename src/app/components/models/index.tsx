055c98893fccceb6d539674800367c1c11165b43
박지원 <jackgg12322@gmail.com>
2024-10-18 02:27:30 +0900

    refactor: "3d model component 동적 함수로 불러오기"

--- src/app/components/models/index.tsx
+++ src/app/components/models/index.tsx
@@ -4,6 +4,7 @@ import { useGLTF } from '@react-three/drei';
 import { GLTF } from 'three-stdlib';
 import { Ref, useCallback, useEffect, useRef } from 'react';
 import { useFrame } from '@react-three/fiber';
+import React from 'react';
 
 type GLTFResult = GLTF & {
   nodes: {
@@ -24,56 +25,61 @@ interface ModelRefProps {
   };
 }
 
-export function PjwCharacter(props: JSX.IntrinsicElements['group']) {
-  const { nodes } = useGLTF('/models/pjw.glb') as unknown as GLTFResult;
+export const PjwCharacter = React.memo(
+  (props: JSX.IntrinsicElements['group']) => {
+    const { nodes } = useGLTF('/models/pjw.glb') as unknown as GLTFResult;
 
-  const modelRef = useRef<ModelRefProps>();
+    const modelRef = useRef<ModelRefProps>();
 
-  useFrame((state) => {
-    if (modelRef.current) {
-      modelRef.current.position.y = Math.sin(state.clock.elapsedTime) * 0.15;
-    }
-  });
+    useFrame((state) => {
+      if (modelRef.current) {
+        modelRef.current.position.y = Math.sin(state.clock.elapsedTime) * 0.15;
+      }
+    });
 
-  const handleMouseMove = (event: MouseEvent) => {
-    if (modelRef.current) {
-      const deltaX = event.movementX * 0.05; // 회전 속도 조절
-      modelRef.current.rotation.y += deltaX; // y축 회전
-    }
-  };
-
-  const handleMouseDown = useCallback(() => {
-    window.addEventListener('mousemove', handleMouseMove);
-  }, []);
-
-  const handleMouseUp = useCallback(() => {
-    window.removeEventListener('mousemove', handleMouseMove);
-  }, []);
-
-  useEffect(() => {
-    window.addEventListener('mousedown', handleMouseDown);
-    window.addEventListener('mouseup', handleMouseUp);
-
-    return () => {
-      window.removeEventListener('mousedown', handleMouseDown);
-      window.removeEventListener('mouseup', handleMouseUp);
+    const handleMouseMove = (event: MouseEvent) => {
+      if (modelRef.current) {
+        const deltaX = event.movementX * 0.05; // 회전 속도 조절
+        modelRef.current.rotation.y += deltaX; // y축 회전
+      }
     };
-  }, [handleMouseDown, handleMouseUp]);
-  return (
-    <group
-      {...props}
-      dispose={null}
-      ref={modelRef as Ref<THREE.Group<THREE.Object3DEventMap>> | undefined}
-    >
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.mesh_0.geometry}
-        material={nodes.mesh_0.material}
-        scale={1.832}
-        onClick={() => handleMouseMove}
-      />
-    </group>
-  );
-}
+
+    const handleMouseDown = useCallback(() => {
+      window.addEventListener('mousemove', handleMouseMove);
+    }, []);
+
+    const handleMouseUp = useCallback(() => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    }, []);
+
+    useEffect(() => {
+      window.addEventListener('mousedown', handleMouseDown);
+      window.addEventListener('mouseup', handleMouseUp);
+
+      return () => {
+        window.removeEventListener('mousedown', handleMouseDown);
+        window.removeEventListener('mouseup', handleMouseUp);
+      };
+    }, [handleMouseDown, handleMouseUp]);
+
+    return (
+      <group
+        {...props}
+        dispose={null}
+        ref={modelRef as Ref<THREE.Group<THREE.Object3DEventMap>> | undefined}
+      >
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.mesh_0.geometry}
+          material={nodes.mesh_0.material}
+          scale={1.832}
+          onClick={() => handleMouseMove}
+        />
+      </group>
+    );
+  }
+);
+PjwCharacter.displayName = 'PjwCharacter';
+export default PjwCharacter;
 useGLTF.preload('/models/pjw.glb');
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import Ripple from '@/app/components/ui/ripple';
-import { PjwCharacter } from './components/models';
 import Navigation from './components/navigation';
 import RenderModel from './components/RenderModel';
+import dynamic from 'next/dynamic';
 
+const PjwCharacter = dynamic(() => import('./components/models'), {
+  ssr: false,
+});
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between relative">
