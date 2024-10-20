264b155f8b73d12773890350713fbb6bd4a91390
박지원 <jackgg12322@gmail.com>
2024-10-20 15:16:52 +0900

    chore:"페이지 내용 수정(career)"

--- src/app/(sub pages)/career/page.tsx
+++ src/app/(sub pages)/career/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function Career() {
-  return <div className="text-gray-500">현재 개발 중인 페이지 입니다.</div>;
+  return <div className="text-gray-500">현재 구직중 입니다.</div>;
 }
