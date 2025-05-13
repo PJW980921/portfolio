369fcdfc5e35a5436e3f5d2761a34d1cfd1e4d1e
박지원 <jackgg12322@gmail.com>
2025-05-13 22:15:28 +0900

    chore : "double quote -> single quote edit"

--- src/app/(sub pages)/career/page.tsx
+++ src/app/(sub pages)/career/page.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { Metadata } from "next";
-import BlurFade from "@/app/components/ui/blur-fade";
-import CareerList from "@/app/components/CareerList";
+import React from 'react';
+import { Metadata } from 'next';
+import BlurFade from '@/app/components/ui/blur-fade';
+import CareerList from '@/app/components/CareerList';
 
 export const metadata: Metadata = {
-  title: "PJW PortFolio - Career",
-  description: "저(박지원)의 경력사항을 확인 할 수 있는 페이지입니다.",
+  title: 'PJW PortFolio - Career',
+  description: '저(박지원)의 경력사항을 확인 할 수 있는 페이지입니다.',
 };
 export default function Career() {
   return (
--- src/app/components/CareerList.tsx
+++ src/app/components/CareerList.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
-import Link from "next/link";
-import { careerList } from "../data";
+import Image from 'next/image';
+import Link from 'next/link';
+import { careerList } from '../data';
 
 export default function CareerList() {
   return (
