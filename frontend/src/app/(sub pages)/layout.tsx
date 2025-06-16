cea78fb1d37a74e28ba80e0930649278821feb5b
박지원 <jackgg12322@gmail.com>
2025-06-16 21:14:45 +0900

    fix:"test commit"

--- frontend/src/app/(sub pages)/layout.tsx
+++ frontend/src/app/(sub pages)/layout.tsx
@@ -3,18 +3,21 @@ import { ReactNode, useEffect } from 'react';
 import HomeButton from '../_components/HomeButton';
 import * as ChannelService from '@channel.io/channel-web-sdk-loader';
 
+ChannelService.loadScript();
+
 interface SubPagesLayoutProps {
   children: ReactNode;
 }
 
 export default function SubPagesLayout({ children }: SubPagesLayoutProps) {
   useEffect(() => {
-    ChannelService.loadScript();
-
-    ChannelService.boot({
-      pluginKey: process.env.NEXT_PUBLIC_CHANNEL_TALK_KEY as string,
-    });
+    if (typeof window !== 'undefined' && window.ChannelIO) {
+      window.ChannelIO('boot', {
+        pluginKey: process.env.NEXT_PUBLIC_CHANNEL_TALK_KEY as string,
+      });
+    }
   }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center px-16">
       <HomeButton />
