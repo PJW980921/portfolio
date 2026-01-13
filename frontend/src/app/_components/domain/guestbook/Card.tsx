8fcb7e2566e40f6b93878493b931541ff501db80
박지원 <jackgg12322@gmail.com>
2026-01-13 17:42:07 +0900

    chore : build error 수정 #2

--- frontend/src/app/_components/domain/guestbook/Card.tsx
+++ frontend/src/app/_components/domain/guestbook/Card.tsx
@@ -1,27 +1,35 @@
-import { CardProps } from '@/app/_data';
-import React from 'react';
-import BlurFade from '../../ui/blur-fade';
+// import { CardProps } from '@/app/_data';
+// import React from 'react';
+// import BlurFade from '../../ui/blur-fade';
 
-const Card = ({ profile, nickname, description }: CardProps) => {
+
+const Card = () => {
   return (
-    <BlurFade delay={0.25} inView>
-      <div className="w-full max-w-[22rem] transition-transform duration-300 ease-in-out hover:scale-95 hover:shadow-lg bg-white rounded-[0.4rem] border border-gray-100">
-        <div className="flex flex-col gap-5 p-4 justify-center items-center overflow-hidden text-ellipsis">
-          <div className="flex flex-row gap-4 items-center">
-            <p className="flex border border-gray-200 rounded-full w-[3.8rem] h-[3.8rem] text-center justify-center text-[2rem] items-center">
-              {profile}
-            </p>
-            <p className="font-bmHannaPro text-[1.6rem] font-extralight text-center">
-              {nickname}
-            </p>
-          </div>
-          <p className="overflow-hidden text-ellipsis font-bmHannaAir text-[1.6rem] font-extralight break-words text-center">
-            {description}
-          </p>
-        </div>
-      </div>
-    </BlurFade>
+    <div>
+      <h1>Card</h1>
+    </div>
   );
 };
+// const Card = ({ profile, nickname, description }: CardProps) => {
+//   return (
+//     <BlurFade delay={0.25} inView>
+//       <div className="w-full max-w-[22rem] transition-transform duration-300 ease-in-out hover:scale-95 hover:shadow-lg bg-white rounded-[0.4rem] border border-gray-100">
+//         <div className="flex flex-col gap-5 p-4 justify-center items-center overflow-hidden text-ellipsis">
+//           <div className="flex flex-row gap-4 items-center">
+//             <p className="flex border border-gray-200 rounded-full w-[3.8rem] h-[3.8rem] text-center justify-center text-[2rem] items-center">
+//               {profile}
+//             </p>
+//             <p className="font-bmHannaPro text-[1.6rem] font-extralight text-center">
+//               {nickname}
+//             </p>
+//           </div>
+//           <p className="overflow-hidden text-ellipsis font-bmHannaAir text-[1.6rem] font-extralight break-words text-center">
+//             {description}
+//           </p>
+//         </div>
+//       </div>
+//     </BlurFade>
+//   );
+// };
 
 export default Card;
