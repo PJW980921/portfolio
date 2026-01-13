1d392ddf8552daf8ccd971ed8ee5335724b5e2b4
박지원 <jackgg12322@gmail.com>
2026-01-13 17:00:53 +0900

    chore : build error 수정

--- frontend/src/app/_components/domain/guestbook/GuestBookList.tsx
+++ frontend/src/app/_components/domain/guestbook/GuestBookList.tsx
@@ -1,33 +1,34 @@
 import React from 'react';
-import Card from './Card';
-import RegisterForm from './RegisterForm';
-import { CardList } from '@/app/_data';
+// import Card from './Card';
+// import RegisterForm from './RegisterForm';
+// import { CardList } from '@/app/_data';
 
 const GuestBookList = () => {
   return (
-    <div className="flex flex-col items-center w-full px-4">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-y-8 max-h-[50rem] overflow-y-scroll scrollbar-hide w-full relative">
-        {CardList.map((item) => (
-          <div
-            key={item.id}
-            className={`
-              w-full flex
-              ${item.id % 2 === 0 ? 'justify-end' : 'justify-start'}
-            `}
-          >
-            <Card
-              id={item.id}
-              profile={item.profile}
-              nickname={item.nickname}
-              description={item.description}
-            />
-          </div>
-        ))}
-      </div>
-      <div className="mt-8 w-full max-w-[30rem]">
-        <RegisterForm />
-      </div>
-    </div>
+    <div></div>
+    // <div className="flex flex-col items-center w-full px-4">
+    //   <div className="grid grid-cols-1 md:grid-cols-2 gap-y-8 max-h-[50rem] overflow-y-scroll scrollbar-hide w-full relative">
+    //     {CardList.map((item) => (
+    //       <div
+    //         key={item.id}
+    //         className={`
+    //           w-full flex
+    //           ${item.id % 2 === 0 ? 'justify-end' : 'justify-start'}
+    //         `}
+    //       >
+    //         <Card
+    //           id={item.id}
+    //           profile={item.profile}
+    //           nickname={item.nickname}
+    //           description={item.description}
+    //         />
+    //       </div>
+    //     ))}
+    //   </div>
+    //   <div className="mt-8 w-full max-w-[30rem]">
+    //     <RegisterForm />
+    //   </div>
+    // </div>
   );
 };
 
